27bed5bf87adfe0f8db35330958baf805a5e8dd0
SwetaPadmaja <swetapadmaja@gmail.com>
2020-11-17 14:03:11 +0530

    Im Sweta

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ function setup() {
   box1=new Box(200,300,50,50);
   box2= new Box(240,100,50,100);
   ground= new Ground(200,390,400,20);
+  box3 = new Box(150,300,50,50);
 }
 
 function draw() {
@@ -18,4 +19,5 @@ function draw() {
   box1.display();
   box2.display();
   ground.display();
+  box3.display();
 }
